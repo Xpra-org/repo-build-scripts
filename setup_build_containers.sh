3bb9d9ee65f9036bc7de38dfe086bf2aa0d53822
totaam <antoine@xpra.org>
2021-10-02 15:57:43 +0700

    when running setup again, we have to ignore the local repo which may have been added already

--- setup_build_containers.sh
+++ setup_build_containers.sh
@@ -60,10 +60,10 @@ for DISTRO in $RPM_DISTROS; do
 			buildah run $IMAGE_NAME dnf config-manager --set-disabled $repo
 		done
 	fi
-	buildah run $IMAGE_NAME $PM update -y
-	buildah run $IMAGE_NAME $PM install -y redhat-rpm-config rpm-build rpmdevtools ${createrepo} rsync
+	buildah run $IMAGE_NAME $PM update --disablerepo=repo-local-source -y
+	buildah run $IMAGE_NAME $PM install --disablerepo=repo-local-source -y redhat-rpm-config rpm-build rpmdevtools ${createrepo} rsync
 	if [ "${PM}" == "dnf" ]; then
-		buildah run $IMAGE_NAME dnf install -y 'dnf-command(builddep)'
+		buildah run $IMAGE_NAME dnf install --disablerepo=repo-local-source -y 'dnf-command(builddep)'
 		buildah run $IMAGE_NAME bash -c "echo 'keepcache=true' >> /etc/dnf/dnf.conf"
 		buildah run $IMAGE_NAME bash -c "echo 'deltarpm=false' >> /etc/dnf/dnf.conf"
 		buildah run $IMAGE_NAME bash -c "echo 'fastestmirror=true' >> /etc/dnf/dnf.conf"
@@ -71,14 +71,14 @@ for DISTRO in $RPM_DISTROS; do
 	if [[ "${DISTRO_LOWER}" == "fedora"* ]]; then
 		RNUM=`echo $DISTRO | awk -F: '{print $2}'`
 		dnf -y makecache --releasever=$RNUM --setopt=cachedir=/var/cache/dnf/$RNUM
-		buildah run $IMAGE_NAME ${PM} install -y rpmspectool
+		buildah run $IMAGE_NAME ${PM} install --disablerepo=repo-local-source -y rpmspectool
 	else
 		#centos8:
 		if [ "${PM}" == "dnf" ]; then
 			#some of the packages we need for building are in the "PowerTools" repository:
 			buildah run $IMAGE_NAME dnf config-manager --set-enabled powertools
 			#no "rpmspectool" package on CentOS 8, use setuptools to install it:
-			buildah run $IMAGE_NAME dnf install -y python3-setuptools
+			buildah run $IMAGE_NAME dnf install --disablerepo=repo-local-source -y python3-setuptools
 			buildah run $IMAGE_NAME easy_install-3.6 python-rpm-spec
 		fi
 	fi
