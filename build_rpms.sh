d5c343a3828e65cd361cdbb7a862ed92cdabca00
totaam <antoine@xpra.org>
2023-03-28 16:40:54 +0700

    #3807 don't rebuild just to get a '.src' rpm
    it may already exist anyway, just not found by the repoquery

--- build_rpms.sh
+++ build_rpms.sh
@@ -81,6 +81,8 @@ while read p; do
 				echo " (ignored debugsource)"
 			elif [[ $dep == *-doc-* ]]; then
 				echo " (ignored doc)"
+			elif [[ $dep == *.src ]]; then
+				echo " (ignored src)"
 			else
 				MISSING="${MISSING} ${dep}"
 			fi
